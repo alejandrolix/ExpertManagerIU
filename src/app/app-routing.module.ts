183bc0f78538b2eeb7d56d0ff391cf315a15d383
Alejandro Pons <alejandro.pons98@gmail.com>
2021-05-08 17:38:00 +0200

    Creada ruta para componente siniestros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SiniestrosComponent } from './siniestros/siniestros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'siniestros',
+    component: SiniestrosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
