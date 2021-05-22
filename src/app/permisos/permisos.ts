fa83e894bd0e81dd389309bc49c89a25e90f9f87
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-05-22 12:32:33 +0200

    Creado método para saber si el usuario tiene permiso perito no responsable

--- src/app/permisos/permisos.ts
+++ src/app/permisos/permisos.ts
@@ -12,4 +12,18 @@ export class Permisos {
         else
             return false;
     }
+
+    public static esPermisoPeritoNoResponsable(): boolean {
+        let idPermiso: string | null = localStorage.getItem('idPermiso');
+
+        if (idPermiso == null)
+            return false;
+
+        let idPermisoNumero: number = parseInt(idPermiso);
+
+        if (idPermisoNumero == 3)
+            return true;
+        else
+            return false;
+    }
 }
