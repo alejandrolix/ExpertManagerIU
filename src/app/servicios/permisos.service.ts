69a372e920237639a5fc4016746e5af73654aa21
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-05-22 16:00:26 +0200

    Creado método tienePermisoAdministracion

--- src/app/servicios/permisos.service.ts
+++ src/app/servicios/permisos.service.ts
@@ -49,4 +49,18 @@ export class PermisosService {
 
     return false;
   }
+
+  public tienePermisoAdministracion(): boolean {
+    let idPermiso: string | null = localStorage.getItem('idPermiso');
+
+    if (idPermiso == null)
+        return false;
+
+    let idPermisoNumero: number = parseInt(idPermiso);
+
+    if (idPermisoNumero == 1)
+        return true;
+    else
+        return false;
+}
 }
