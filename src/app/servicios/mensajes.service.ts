47d4ae6fb249dc4be7932ea52ae19a53b464123a
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-05-22 17:05:13 +0200

    Creado método mensajesService para obtener mensajes por id siniestro

--- src/app/servicios/mensajes.service.ts
+++ src/app/servicios/mensajes.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Mensaje } from '../interfaces/mensaje';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class MensajesService {
 
   public crear(mensaje: any): Observable<boolean> {
     return this.http.post<boolean>(`${environment.urlApi}/Mensajes`, mensaje);
+  }
+
+  public obtenerTodosPorIdSiniestro(idSiniestro: number): Observable<Mensaje[]> {
+    return this.http.get<Mensaje[]>(`${environment.urlApi}/Mensajes`);
   }
 }
