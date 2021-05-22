a3a5c7ea33c4e59aa0c5f1d0bd6312423371143e
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-05-22 17:02:40 +0200

    Implementada lógica para crear mensajes

--- src/app/siniestros/crear-mensaje/crear-mensaje.component.ts
+++ src/app/siniestros/crear-mensaje/crear-mensaje.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MensajesService } from 'src/app/servicios/mensajes.service';
+import { UsuariosService } from 'src/app/servicios/usuarios.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-crear-mensaje',
@@ -9,18 +12,62 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CrearMensajeComponent implements OnInit {
   public formCrearMensaje: FormGroup;
+  private idSiniestro: number;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private usuariosService: UsuariosService, private mensajesService: MensajesService,
+              private router: Router) { }
 
   ngOnInit(): void {
-    let idSiniestro: number = Number(this.route.snapshot.paramMap.get('id'));
+    this.idSiniestro = Number(this.route.snapshot.paramMap.get('id'));
 
     this.formCrearMensaje = new FormGroup({
       descripcion: new FormControl('', Validators.required)
     });
   }
 
-  public crear(): void {
-    
+  public async crear(): Promise<void> {
+    if (!this.formCrearMensaje.valid)
+      return;
+
+    let descripcion: string = this.formCrearMensaje.get('descripcion')?.value;
+    let idUsuarioCreado: number = this.usuariosService.obtenerIdUsuarioLogueado();
+    let idSiniestro: number = this.idSiniestro;
+
+    let mensaje = {
+      descripcion: descripcion,
+      idUsuarioCreado: idUsuarioCreado,
+      idSiniestro: idSiniestro
+    };
+
+    let respuesta: boolean = await this.mensajesService.crear(mensaje).toPromise();
+
+    if (respuesta) {
+      let accion = await Swal.fire({
+        title: 'Mensaje creado correctamente',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        },
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
+      });
+
+      if (accion.isConfirmed)
+        this.router.navigate(['/detallesSiniestro', this.idSiniestro]);
+    }     
+    else
+      Swal.fire({
+        title: 'Ha habido un error al crear el mensaje',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        },
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      });
   }
 }
