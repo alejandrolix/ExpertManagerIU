9d65bc54ea44858b382ced2064fd993698d91a97
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-05-22 08:24:40 +0200

    Creado lógica para mostrar login o app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'IU';
+  public estaSesionIniciada: boolean;
+
+  constructor() {
+    this.estaSesionIniciada = false;
+  }
 }
