57074f37a09ed9a364de5f978094baa6f474d5cf
Alejandro Pons <alejandro.pons98@gmail.com>
2021-05-25 20:30:11 +0200

    Mostrado tarjetas con estadísticas

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InicioService } from 'src/app/servicios/inicio.service';
+import { PermisosService } from 'src/app/servicios/permisos.service';
+import { UsuariosService } from 'src/app/servicios/usuarios.service';
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent implements OnInit {
+  public estadisticas: any;
+  public mostrarSpinner: boolean;
+  public tieneUsuarioPermisoAdministracion: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private inicioService: InicioService, private usuariosService: UsuariosService, private permisosService: PermisosService) {
+    this.mostrarSpinner = false;
   }
 
+  async ngOnInit(): Promise<void> {
+    this.mostrarSpinner = true;
+    let idUsuarioLogueado: number = this.usuariosService.obtenerIdUsuarioLogueado();
+    this.tieneUsuarioPermisoAdministracion = this.permisosService.tienePermisoAdministracion();
+
+    this.estadisticas = await this.inicioService.obtenerEstadisticasPorIdUsuario(idUsuarioLogueado).toPromise();
+    console.log(this.estadisticas)
+
+    this.mostrarSpinner = false;
+  }
 }
