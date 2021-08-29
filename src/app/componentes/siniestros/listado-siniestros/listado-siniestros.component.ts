5c729b634007b3c958a45bac0fbf273b9e98fd97
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-08-29 10:48:41 +0200

    Usado promesa método "obtenerPorPeritoResponsable" siniestros

--- src/app/componentes/siniestros/listado-siniestros/listado-siniestros.component.ts
+++ src/app/componentes/siniestros/listado-siniestros/listado-siniestros.component.ts
@@ -167,9 +167,12 @@ export class ListadoSiniestrosComponent implements OnInit {
       let idPerito: number = this.usuariosService.obtenerIdUsuarioLogueado();
 
       if (this.permisosService.tienePermisoPeritoResponsable())
-        this.siniestrosService.obtenerPorPeritoResponsable(idPerito, this.idAseguradoraSeleccionada)
-                              .subscribe((siniestros: Siniestro[]) => this.siniestros = siniestros,
-                              (mensaje: string) => Alerta.mostrarError(mensaje));       
+        try {
+          this.siniestros = await this.siniestrosService.obtenerPorPeritoResponsable(idPerito, this.idAseguradoraSeleccionada)
+                                      .toPromise();
+        } catch (error) {
+          Alerta.mostrarError(error);
+        }
       else
         this.siniestrosService.obtenerPorPeritoNoResponsable(idPerito, this.idAseguradoraSeleccionada)
                               .subscribe((siniestros: Siniestro[]) => this.siniestros = siniestros,
