a68c80b5d7a20b30bb4a51f8d548cc0d1065bab5
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-02 20:24:38 +0200

    Refactorizado mostrar error si usuario no tiene permiso para navegar por rutas

--- src/app/guards/acceder-ruta.guard.ts
+++ src/app/guards/acceder-ruta.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import Swal, { SweetAlertResult } from 'sweetalert2';
+import { Alerta } from '../clases/Alerta';
 import { PermisosService } from '../servicios/permisos.service';
 
 @Injectable({
@@ -11,29 +11,17 @@ export class AccederRutaGuard implements CanActivate {
   
   constructor(private permisosService: PermisosService) { }
 
-  canActivate(route: ActivatedRouteSnapshot,
-              state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       
     if (this.permisosService.tienePermisoAdministracion())
       return true;
 
-    return new Observable(observer => {
-      Swal.fire({
-        title: 'No puede acceder a esta sección porque no tiene el permiso adecuado',
-        icon: 'error',          
-        confirmButtonColor: '#3085d6',          
-        confirmButtonText: 'Aceptar',          
-      })
-      .then((accion: SweetAlertResult) => {
-        if (accion.isConfirmed) {
-          observer.next(false);
-          observer.complete();
-
-          return false;
-        }          
-
-        return true;
-      });
+    return new Observable(() => {
+      Alerta.mostrarErrorAsincrono('No puede acceder a esta sección porque no tiene el permiso adecuado')
+            .then(() => {
+              history.back();
+              return false;
+            });
     });
   }
 }
