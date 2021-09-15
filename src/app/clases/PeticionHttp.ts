48ecd04e30a943984f4422d62040c12785f8d21c
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-15 18:25:19 +0200

    eliminado imports innecesarios clase PeticionHttp

--- src/app/clases/PeticionHttp.ts
+++ src/app/clases/PeticionHttp.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, throwError } from "rxjs";
-import { map, catchError, tap } from "rxjs/operators";
-import { RespuestaApi } from "../interfaces/respuestaApi";
+import { catchError } from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
