c7edaa3689693dc8aa889d420aae6933d53944d2
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-15 18:14:13 +0200

    Refactorizado método hacerPeticionPut

--- src/app/clases/PeticionHttp.ts
+++ src/app/clases/PeticionHttp.ts
@@ -39,22 +39,17 @@ export class PeticionHttp {
     }
 
     public hacerPeticionPut<T>(url: string, datos: any): Observable<T> {
-        return this.http.put<RespuestaApi>(url, datos)
-            .pipe(
-                tap((respuesta: RespuestaApi) => {
-                    if (respuesta.codigoRespuesta === 500 && respuesta.mensaje)
-                        throw new Error(respuesta.mensaje);     // El mensaje del error se procesa en la función "catchError".
-
-                    return throwError(null);
-                }),
-                map((respuesta: RespuestaApi) => respuesta.datos),
-                catchError((error: Error) => {
-                    if (error.message)
-                        return throwError(error.message);
-
-                    return throwError('Ha habido un error al actualizar');
-                })
-            );
+        return this.http.put<T>(url, datos)
+                        .pipe(
+                            catchError((error: any) => {                    
+                                if (error.error === 'no token')
+                                    return throwError('No existe token. Por favor, inicie sesión');
+                                else if (error.status === 0)                    
+                                    return throwError('No funciona la API REST');
+                                
+                                return throwError(error.error);
+                            })
+                        );
     }
 
     public hacerPeticionDelete<T>(url: string): Observable<T> {
