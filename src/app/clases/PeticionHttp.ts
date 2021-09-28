f22066cf3b05b8016274fea83e8370aef9b33253
Alejandro Pons <alejandro.pons98@gmail.com>
2021-09-28 21:54:26 +0200

    Creado función obtenerMensajeError y usado en hacerPeticionGet

--- src/app/clases/PeticionHttp.ts
+++ src/app/clases/PeticionHttp.ts
@@ -12,15 +12,17 @@ export class PeticionHttp {
     public hacerPeticionGet<T>(url: string): Observable<T> {
         return this.http.get<T>(url)
                         .pipe(
-                            catchError((error: any) => {                    
-                                if (error.error === 'no token')
-                                    return throwError('No existe token. Por favor, inicie sesión');
-                                else if (error.status === 0)                    
-                                    return throwError('No funciona la API REST');
-                                
-                                return throwError(error.error);
-                            })
+                            catchError((error: any) => this.obtenerMensajeError(error))
                         );
+    }
+
+    private obtenerMensajeError(error: any): Observable<never> {
+        if (error.error === 'no token')
+            return throwError('No existe token. Por favor, inicie sesión');
+        else if (error.status === 0)                    
+            return throwError('No funciona la API REST');
+    
+        return throwError(error.error);
     }
 
     public hacerPeticionGetConOpciones(url: string, opciones: {
