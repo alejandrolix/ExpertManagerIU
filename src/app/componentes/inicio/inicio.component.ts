3203fa9a70eac07a328b4388f5fb2d1aea634c53
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-10-11 20:23:15 +0200

    Cambiado obtener id usuario en componente inicio

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Alerta } from 'src/app/clases/Alerta';
 import { Estadistica } from 'src/app/interfaces/estadistica';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 import { InicioService } from 'src/app/servicios/inicio.service';
 import { PermisosService } from 'src/app/servicios/permisos.service';
 import { SpinnerService } from 'src/app/servicios/spinner.service';
-import { UsuariosService } from 'src/app/servicios/usuarios.service';
 
 @Component({
   selector: 'app-inicio',
@@ -15,16 +15,17 @@ export class InicioComponent implements OnInit {
   public estadisticas: Estadistica;
   public tieneUsuarioPermisoAdministracion: boolean;
 
-  constructor(private inicioService: InicioService, private usuariosService: UsuariosService, private permisosService: PermisosService,
-              private spinnerService: SpinnerService) { }
+  constructor(private inicioService: InicioService, private permisosService: PermisosService, private spinnerService: SpinnerService,
+              private autenticacionService: AutenticacionService) { }
 
   async ngOnInit(): Promise<void> {    
-    this.spinnerService.mostrarSpinner();
-    let idUsuarioLogueado: number = this.usuariosService.obtenerIdUsuarioLogueado();
+    let idUsuario: number = this.autenticacionService.obtenerIdUsuario();
+
+    this.spinnerService.mostrarSpinner();    
     this.tieneUsuarioPermisoAdministracion = this.permisosService.tienePermisoAdministracion();
 
     try {
-      this.estadisticas = await this.inicioService.obtenerEstadisticasPorIdUsuario(idUsuarioLogueado)
+      this.estadisticas = await this.inicioService.obtenerEstadisticasPorIdUsuario(idUsuario)
                                                   .toPromise();
     } catch (error: any) {
       Alerta.mostrarError(error);
