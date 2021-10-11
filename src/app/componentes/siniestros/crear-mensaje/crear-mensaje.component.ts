b9e1f5f8a104a4e671b7dc1c148343d8cb3f8eb3
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2021-10-11 20:27:42 +0200

    Cambiado obtener id usuario en componente crear mensaje

--- src/app/componentes/siniestros/crear-mensaje/crear-mensaje.component.ts
+++ src/app/componentes/siniestros/crear-mensaje/crear-mensaje.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Alerta } from 'src/app/clases/Alerta';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 import { MensajesService } from 'src/app/servicios/mensajes.service';
 import { SpinnerService } from 'src/app/servicios/spinner.service';
-import { UsuariosService } from 'src/app/servicios/usuarios.service';
 
 @Component({
   selector: 'app-crear-mensaje',
@@ -15,7 +15,7 @@ export class CrearMensajeComponent implements OnInit {
   public formCrearMensaje: FormGroup;
   private idSiniestro: number;
 
-  constructor(private route: ActivatedRoute, private usuariosService: UsuariosService, private mensajesService: MensajesService,
+  constructor(private route: ActivatedRoute, private autenticacionService: AutenticacionService, private mensajesService: MensajesService,
               private router: Router, private spinnerService: SpinnerService) { }
 
   ngOnInit(): void {
@@ -32,13 +32,13 @@ export class CrearMensajeComponent implements OnInit {
       return;
 
     let descripcion: string = this.formCrearMensaje.get('descripcion')?.value;
-    let idUsuarioCreado: number = this.usuariosService.obtenerIdUsuarioLogueado();
+    let idUsuarioCreado: number = this.autenticacionService.obtenerIdUsuario();
     let idSiniestro: number = this.idSiniestro;
 
-    let mensaje = {
-      descripcion: descripcion,
-      idUsuarioCreado: idUsuarioCreado,
-      idSiniestro: idSiniestro
+    let mensaje: { descripcion: string, idUsuarioCreado: number, idSiniestro: number } = {
+      descripcion,
+      idUsuarioCreado,
+      idSiniestro
     };
 
     this.spinnerService.mostrarSpinner();
