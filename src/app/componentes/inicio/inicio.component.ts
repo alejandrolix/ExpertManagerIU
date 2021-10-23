1cf884d4ca66806e1947821243b0bea408f24331
Alejandro Pons <alejandro.pons98@gmail.com>
2021-10-23 11:43:24 +0200

    Eliminado uso servicio signalR en componente inicio

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -4,7 +4,6 @@ import { Estadistica } from 'src/app/interfaces/estadistica';
 import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 import { InicioService } from 'src/app/servicios/inicio.service';
 import { PermisosService } from 'src/app/servicios/permisos.service';
-import { SignalRService } from 'src/app/servicios/signal-r.service';
 import { SpinnerService } from 'src/app/servicios/spinner.service';
 
 @Component({
@@ -16,8 +15,7 @@ export class InicioComponent implements OnInit {
   public estadistica: Estadistica;
   public tieneUsuarioPermisoAdministracion: boolean;
 
-  constructor(private autenticacionService: AutenticacionService, private permisosService: PermisosService, private spinnerService: SpinnerService, private inicioService: InicioService,
-              private signalRService: SignalRService) { }
+  constructor(private autenticacionService: AutenticacionService, private permisosService: PermisosService, private spinnerService: SpinnerService, private inicioService: InicioService) { }
 
   async ngOnInit(): Promise<void> {
     let idUsuario: number = this.autenticacionService.obtenerIdUsuario();
