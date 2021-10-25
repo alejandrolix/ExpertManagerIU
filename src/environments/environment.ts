5b359d4748b7488272ce5584869d92da71e566d5
Alejandro Pons <alejandro.pons98@gmail.com>
2021-10-25 18:41:34 +0200

    Usada directiva appNumerosDecimales en componente crear y editar usuario

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  urlApi: 'https://localhost:44345/api',
+  urlApi: 'https://localhost:5001/api',
   urlHub: 'https://localhost:44345/mensajes'
 };
 
