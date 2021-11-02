c5e4b24f9daefb07efda76f98f70c27e9328ec35
Alejandro Pons <alejandro.pons98@gmail.com>
2021-11-02 18:35:17 +0100

    Eliminado comentarios y variables innecesarias

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Alerta } from 'src/app/clases/Alerta';
 import { Estadistica } from 'src/app/interfaces/estadistica';
 import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 import { InicioService } from 'src/app/servicios/inicio.service';
 import { PermisosService } from 'src/app/servicios/permisos.service';
-import { SpinnerService } from 'src/app/servicios/spinner.service';
 
 @Component({
   selector: 'app-inicio',
@@ -15,21 +13,13 @@ export class InicioComponent implements OnInit {
   public estadistica: Estadistica;
   public tieneUsuarioPermisoAdministracion: boolean;
 
-  constructor(private autenticacionService: AutenticacionService, private permisosService: PermisosService, private spinnerService: SpinnerService, private inicioService: InicioService) { }
+  constructor(private autenticacionService: AutenticacionService, private permisosService: PermisosService, private inicioService: InicioService) { }
 
   async ngOnInit(): Promise<void> {
     let idUsuario: number = this.autenticacionService.obtenerIdUsuario();
 
-    // this.spinnerService.mostrarSpinner();
     this.tieneUsuarioPermisoAdministracion = this.permisosService.tienePermisoAdministracion();
-
-    // try {
-      this.estadistica = await this.inicioService.obtenerEstadisticasPorIdUsuario(idUsuario)
-                                                 .toPromise();
-    // } catch (error: any) {
-    //   Alerta.mostrarError(error);
-    // }
-
-    // this.spinnerService.ocultarSpinner();
+    this.estadistica = await this.inicioService.obtenerEstadisticasPorIdUsuario(idUsuario)
+                                                .toPromise();
   }
 }
