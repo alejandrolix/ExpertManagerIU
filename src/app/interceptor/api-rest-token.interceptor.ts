eb7b5d35bdf0eac45ccca2ea6b1a7dc239ec7830
Alejandro Pons Antelo <alejandro.pons98@gmail.com>
2023-02-12 12:51:37 +0100

    Eliminado mostrar y ocultar spinner api-rest-token interceptor

--- src/app/interceptor/api-rest-token.interceptor.ts
+++ src/app/interceptor/api-rest-token.interceptor.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, finalize } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { Alerta } from '../clases/Alerta';
-import { SpinnerService } from '../servicios/spinner.service';
 import { AutenticacionService } from '../servicios/autenticacion.service';
 
 @Injectable()
 export class ApiRestTokenInterceptor implements HttpInterceptor {
 
-  constructor(private spinnerService: SpinnerService, private autenticacionService: AutenticacionService) {}
+  constructor(private autenticacionService: AutenticacionService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let enviarTokenEnPeticion: boolean;
@@ -36,10 +30,7 @@ export class ApiRestTokenInterceptor implements HttpInterceptor {
         }
       });
 
-    this.spinnerService.mostrarSpinner();
-
     return next.handle(request).pipe(
-      finalize(() => this.spinnerService.ocultarSpinner()),
       catchError((error: any) => this.mostrarError(error))
     );
   }
